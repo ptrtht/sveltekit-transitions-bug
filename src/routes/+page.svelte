887b5a27c55484594f5dd835dc07d6f4fd854f46
qty0 <peterszabototh@gmail.com>
2024-01-04 18:20:19 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2000);
+  });
+</script>
+
+{#await promise}
+  <!--  -->
+{:then value}
+  {#if value}
+    <button in:fade={{ duration: 2000 }}> This is a button </button>
+  {/if}
+{/await}
